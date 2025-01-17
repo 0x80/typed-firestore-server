11099e8b8edadb8d2470390171c2114eb4c5aa9a
Thijs Koerselman <thijskoerselman@gmail.com>
2025-01-17 13:13:59 +0100

    Add export for specific documents

--- src/documents/index.ts
+++ src/documents/index.ts
@@ -1,5 +1,6 @@
 export * from "./get-document";
 export * from "./get-document-data";
+export * from "./get-specific-document";
 export * from "./make-document";
 export * from "./make-human-readable-document";
 export * from "./make-mutable-document";

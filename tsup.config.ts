7da508a048d26228064cb1706c19576ad259a3e9
Thijs Koerselman <thijskoerselman@gmail.com>
2025-01-20 12:37:21 +0100

    Fix bundling and export settings

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: {
     index: "src/index.ts",
-    functions: "src/functions/index.ts",
+    "functions/index": "src/functions/index.ts",
   },
   format: "esm",
   target: "esnext",

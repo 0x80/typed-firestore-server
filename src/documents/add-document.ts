9d39e2f6a85a608c7d1267b23fdac6b76dda1503
Thijs Koerselman <thijskoerselman@gmail.com>
2025-12-02 10:09:50 +0100

    Add addDocumentTx for transaction support

--- src/documents/add-document.ts
+++ src/documents/add-document.ts
@@ -1,14 +1,30 @@
 import {
   CollectionReference,
   type DocumentData,
+  type Transaction,
   type WithFieldValue,
 } from "firebase-admin/firestore";
 
+/** Add a new document to a collection with an auto-generated ID. */
 export async function addDocument<T extends DocumentData>(
   ref: CollectionReference<T>,
   data: WithFieldValue<T>
 ): Promise<string> {
   const docRef = await ref.add(data);
 
+  return docRef.id;
+}
+
+/**
+ * Add a new document to a collection with an auto-generated ID within a
+ * transaction.
+ */
+export function addDocumentTx<T extends DocumentData>(
+  tx: Transaction,
+  ref: CollectionReference<T>,
+  data: WithFieldValue<T>
+): string {
+  const docRef = ref.doc();
+  tx.create(docRef, data);
   return docRef.id;
 }

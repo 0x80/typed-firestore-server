1de11a6b1963d1c522d08bdd0c5fb95120040d99
Thijs Koerselman <thijskoerselman@gmail.com>
2026-02-17 14:47:46 +0100

    Set minimum Node.js version to 22

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     "functions/index": "src/functions/index.ts",
   },
   format: ["esm"],
-  target: "node20",
+  target: "node22",
   sourcemap: true,
   treeshake: true,
   dts: true,
